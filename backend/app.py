be4a47857a10b7ec3ee639c6560fb2d161d293fb
siva-insights <sivashanmugam026@gmail.com>
2026-06-20 22:59:17 -0500

    Disable logging for uvicorn server

--- backend/app.py
+++ backend/app.py
@@ -130,4 +130,4 @@ def open_browser():
 
 if __name__ == "__main__":
     threading.Thread(target=open_browser).start()
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_config=None)
